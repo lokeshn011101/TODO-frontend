8af29547c580d416b4b69d52b343655675b704f0
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-05-14 22:25:42 +0530

    changed actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,19 @@ import { GET_DATA } from "./types";
 import axios from "axios";
 
 export const getData = () => async (dispatch) => {
-  const res = await axios.get("http://localhost:5000/todos/");
+  const res = await axios.get(
+    "https://todo-backend-lokesh.herokuapp.com//todos/"
+  );
   dispatch({ type: GET_DATA, payload: res.data });
 };
 
 export const addTask = (dt, txt) => async (dispatch) => {
-  const res = await axios.post("http://localhost:5000/todos/", {
-    due_by: dt,
-    task: txt,
-  });
+  const res = await axios.post(
+    "https://todo-backend-lokesh.herokuapp.com//todos/",
+    {
+      due_by: dt,
+      task: txt,
+    }
+  );
   dispatch({ type: GET_DATA, payload: res.data });
 };
